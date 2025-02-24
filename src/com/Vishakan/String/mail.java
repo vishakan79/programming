b845794950d2769586de7fb6025d9c7c7f047338
Vishakan S <161746996+vishakan79@users.noreply.github.com>
2025-02-24 14:34:32 +0530

    Programming

--- src/com/Vishakan/String/mail.java
+++ src/com/Vishakan/String/mail.java
@@ -1,7 +1,133 @@
-package com.Vishakan.String;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
 
-public class mail {
+public class mail extends Frame implements KeyListener, Runnable {
+    private static final int TILE_SIZE = 20;
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 400;
+
+    private int[] x = new int[400], y = new int[400];
+    private int length = 5;
+    private char direction = 'R';
+    private boolean running = true;
+
+    private int foodX, foodY;
+
+    public mail() {
+        this.setTitle("Snake Game");
+        this.setSize(WIDTH, HEIGHT);
+        this.setVisible(true);
+        this.setResizable(false);
+        this.addKeyListener(this);
+
+        // Initialize Snake
+        for (int i = 0; i < length; i++) {
+            x[i] = 100 - i * TILE_SIZE;
+            y[i] = 100;
+        }
+
+        spawnFood();
+
+        Thread gameThread = new Thread(this);
+        gameThread.start();
+
+        // Close window event
+        this.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                dispose();
+                System.exit(0);
+            }
+        });
+    }
+
+    public void spawnFood() {
+        Random random = new Random();
+        foodX = random.nextInt(WIDTH / TILE_SIZE) * TILE_SIZE;
+        foodY = random.nextInt(HEIGHT / TILE_SIZE) * TILE_SIZE;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        if (running) {
+            g.setColor(Color.RED);
+            g.fillRect(foodX, foodY, TILE_SIZE, TILE_SIZE);
+
+            for (int i = 0; i < length; i++) {
+                g.setColor(i == 0 ? Color.GREEN : Color.CYAN);
+                g.fillRect(x[i], y[i], TILE_SIZE, TILE_SIZE);
+            }
+        } else {
+            g.setColor(Color.RED);
+            g.setFont(new Font("Arial", Font.BOLD, 30));
+            g.drawString("Game Over!", WIDTH / 4, HEIGHT / 2);
+        }
+    }
+
+    public void move() {
+        for (int i = length; i > 0; i--) {
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
+        }
+        switch (direction) {
+            case 'U': y[0] -= TILE_SIZE; break;
+            case 'D': y[0] += TILE_SIZE; break;
+            case 'L': x[0] -= TILE_SIZE; break;
+            case 'R': x[0] += TILE_SIZE; break;
+        }
+    }
+
+    public void checkCollision() {
+        if (x[0] < 0 || x[0] >= WIDTH || y[0] < 0 || y[0] >= HEIGHT) {
+            running = false;
+        }
+        for (int i = length; i > 0; i--) {
+            if (x[0] == x[i] && y[0] == y[i]) {
+                running = false;
+            }
+        }
+    }
+
+    public void checkFood() {
+        if (x[0] == foodX && y[0] == foodY) {
+            length++;
+            spawnFood();
+        }
+    }
+
+    @Override
+    public void run() {
+        while (running) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            move();
+            checkFood();
+            checkCollision();
+            repaint();
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_LEFT: if (direction != 'R') direction = 'L'; break;
+            case KeyEvent.VK_RIGHT: if (direction != 'L') direction = 'R'; break;
+            case KeyEvent.VK_UP: if (direction != 'D') direction = 'U'; break;
+            case KeyEvent.VK_DOWN: if (direction != 'U') direction = 'D'; break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
     public static void main(String[] args) {
-        System.out.println("hello world");
+        new mail();
     }
 }
+
